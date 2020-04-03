582f9ff5a8d583e1b2696a5981c8daddcb4ddd31
Sergei G <srgthe@gmail.com>
2020-04-03 12:57:00 -0700

    deleted scrollbar component

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ function App() {
       <AppWrapper>
         <GlobalStyle />
         <Router>
-          {/*           <ScrollHandler /> */}
           <QueryParamProvider ReactRouterRoute={Route}>
             <Header />
             <AppMainWrapper>
