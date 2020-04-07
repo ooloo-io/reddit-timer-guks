ba02099628b0f7fec72cf02f42a999e6d5df6ee4
Sergei G <srgthe@gmail.com>
2020-04-07 11:06:11 -0700

    added fetch posts from reddit API on form submit

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useQueryParam, StringParam } from 'use-query-params';
+import fetchPosts from '../../api/fetchPosts';
 import SearchForm from '../../components/SearchForm/SearchForm';
 import Spinner from '../../components/Spinner/Spinner';
 import { SearchPage } from './Search.styles';
 
-const Search = () => (
-  <SearchPage>
-    <SearchForm />
-    <Spinner />
-  </SearchPage>
-);
+const Search = () => {
+  const [loading, setLoading] = useState(false);
+  const [subreddit, setSubreddit] = useQueryParam('q', StringParam);
 
+  useEffect(() => {
+    setLoading(true);
+    const fetchData = async () => {
+      await fetchPosts(subreddit);
+      setLoading(false);
+    };
+    fetchData();
+  }, [subreddit]);
+
+  return (
+    <SearchPage>
+      <SearchForm subreddit={subreddit} setSubreddit={setSubreddit} />
+      {loading && <Spinner />}
+    </SearchPage>
+  );
+};
 
 export default Search;
